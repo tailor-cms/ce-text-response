2795998952874dc14b4d43d9636347059f12069c
rkusan00 <renato.kusanovic@gmail.com>
2025-07-02 16:44:38 +0200

    Migrate Display package

--- packages/display/src/components/Display.vue
+++ packages/display/src/components/Display.vue
@@ -1,6 +1,6 @@
 <template>
   <QuestionContainer
-    :data="data"
+    :data="element.data"
     :is-correct="userState.isCorrect"
     :is-graded="isGraded"
     :is-submitted="isSubmitted"
@@ -24,10 +24,10 @@
 
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue';
-import { ElementData } from '@tailor-cms/ce-text-response-manifest';
+import { Element } from '@tailor-cms/ce-text-response-manifest';
 import { QuestionContainer } from '@tailor-cms/lx-components';
 
-const props = defineProps<{ id: number; data: ElementData; userState: any }>();
+const props = defineProps<{ element: Element; userState: any }>();
 const emit = defineEmits(['interaction']);
 
 const isSubmitted = ref(!!props.userState.isSubmitted);
